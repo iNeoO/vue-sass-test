b115c918d5bca349ff6fa03a60f9360d9aa2792a
valère pique <valere@harfanglab.fr>
2022-07-05 17:43:49 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ color }}
+  </div>
+</template>
+
+<script>
+import colors from './assets/_colors.scss';
+
+export default {
+  name: 'App',
+  computed: {
+    color() {
+      return colors.white;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
